3ab5d5e7a31dd1176363cc98cb45d9bce7b9a543
Guilherme Uhrigshardt <guilhermeuh@gmail.com>
2023-12-12 23:06:32 -0300

    service running first API endpoints

--- obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7415c369842d3068340d0ae47f3268c3533bb32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd2ceb3d3390583dcce14c4f8266b88e56b5b468")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
